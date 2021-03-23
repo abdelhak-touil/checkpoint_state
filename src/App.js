2b2c838431be5a39200ed164c99907ef75dd342c
abdelhak-touil <abdelhak.touil1994@gmail.com>
2021-03-23 15:14:06 +0100

    checkpoint state modified

--- src/App.js
+++ src/App.js
@@ -9,7 +9,13 @@ export default class App extends Component {
 
     this.state = {
       show: false,
+      date: new Date(),
     };
+  }
+  componentDidMount() {
+    this.intervall = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, 1000);
   }
 
   render() {
@@ -18,6 +24,7 @@ export default class App extends Component {
         <button onClick={() => this.setState({ show: !this.state.show })}>
           Show
         </button>
+        <h1>{this.state.date.toLocaleTimeString()}</h1>
         {this.state.show ? <File /> : null}
       </div>
     );
--- src/File.js
+++ src/File.js
@@ -1,24 +1,27 @@
 import React, { Component } from "react";
-import logo from "../src/Capture6.PNG";
+import logo from "./Capture6.PNG";
 
 export default class File extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      fullName: "TOUIL Abdelhak",
-      profession: "Web Developer",
-      bio: "I'm a web developer",
-      imgSrc: { logo },
+      person: {
+        fullName: "TOUIL Abdelhak",
+        profession: "Web Developer",
+        bio: "I'm a web developer",
+        imgSrc: logo,
+      },
     };
   }
+
   render() {
     return (
       <div>
-        <h1>{this.state.fullName}</h1>
-        {this.state.imgSrc}
-        <h2>{this.state.profession}</h2>
-        <h3>{this.state.bio}</h3>
+        <h1>{this.state.person.fullName}</h1>
+        <img src={this.state.person.imgSrc} alt="Pic" />
+        <h2>{this.state.person.profession}</h2>
+        <h3>{this.state.person.bio}</h3>
       </div>
     );
   }
